a7470d93724001e45b46d2ef83305b473c910bf7
ShooSh7 <moh.shanaah@gmail.com>
2015-11-09 12:21:34 +0100

    project

---
+++ TestWPF/war/js/checkCheckbox.js
@@ -0,0 +1,114 @@
+function myFunction() {
+    	  if(document.getElementById("1").checked){
+    	 		document.getElementById("1").disabled = true;
+    		}
+    	  if(document.getElementById("2").checked){
+    			 document.getElementById("2").disabled = true;
+    		}
+    	  if(document.getElementById("3").checked){
+       			 document.getElementById("3").disabled = true;
+       		}
+    	  if(document.getElementById("4").checked){
+    	       	 document.getElementById("4").disabled = true;
+    	       	}
+    	  if(document.getElementById("5").checked){
+    	       	 document.getElementById("5").disabled = true;
+    	       	}  
+    	  if(document.getElementById("6").checked){
+    	       	 document.getElementById("6").disabled = true;
+    	       	}
+    	  if(document.getElementById("7").checked){
+ 	       	 document.getElementById("7").disabled = true;
+ 	       	}
+    	  if(document.getElementById("8").checked){
+ 	       	 document.getElementById("8").disabled = true;
+ 	       	}
+    	  if(document.getElementById("9").checked){
+ 	       	 document.getElementById("9").disabled = true;
+ 	       	}
+    	  if(document.getElementById("10").checked){
+  	 		document.getElementById("10").disabled = true;
+  			}
+  		  if(document.getElementById("11").checked){
+  			 document.getElementById("11").disabled = true;
+  			}
+  	  		if(document.getElementById("12").checked){
+     	 		document.getElementById("12").disabled = true;
+     		}
+  	  		if(document.getElementById("13").checked){
+  	       	 	document.getElementById("13").disabled = true;
+  	       	}
+  	  		if(document.getElementById("14").checked){
+  	       		 document.getElementById("14").disabled = true;
+  	       	}  
+  	  		if(document.getElementById("15").checked){
+  	       		 document.getElementById("15").disabled = true;
+  	       	}
+  	 		 if(document.getElementById("16").checked){
+	       	 document.getElementById("16").disabled = true;
+	       	}
+  	 		 if(document.getElementById("17").checked){
+	       	 document.getElementById("17").disabled = true;
+	       	}
+  			  if(document.getElementById("18").checked){
+	       	 document.getElementById("18").disabled = true;
+	       	}
+    	}
+
+function myFunction2() {
+	  if(document.getElementById("1").checked){
+	 		document.getElementById("1").disabled = false;
+		}
+	  if(document.getElementById("2").checked){
+			 document.getElementById("2").disabled = false;
+		}
+	  if(document.getElementById("3").checked){
+ 			 document.getElementById("3").disabled = false;
+ 		}
+	  if(document.getElementById("4").checked){
+	       	 document.getElementById("4").disabled = false;
+	       	}
+	  if(document.getElementById("5").checked){
+	       	 document.getElementById("5").disabled = false;
+	       	}  
+	  if(document.getElementById("6").checked){
+	       	 document.getElementById("6").disabled = false;
+	       	}
+	  if(document.getElementById("7").checked){
+      	 document.getElementById("7").disabled = false;
+      	}
+	  if(document.getElementById("8").checked){
+      	 document.getElementById("8").disabled = false;
+      	}
+	  if(document.getElementById("9").checked){
+      	 document.getElementById("9").disabled = false;
+      	}
+	  if(document.getElementById("10").checked){
+ 		document.getElementById("10").disabled = false;
+		}
+	  if(document.getElementById("11").checked){
+		 document.getElementById("11").disabled = false;
+		}
+  		if(document.getElementById("12").checked){
+	 		document.getElementById("12").disabled = false;
+		}
+  		if(document.getElementById("13").checked){
+       	 	document.getElementById("13").disabled = false;
+       	}
+  		if(document.getElementById("14").checked){
+       		 document.getElementById("14").disabled = false;
+       	}  
+  		if(document.getElementById("15").checked){
+       		 document.getElementById("15").disabled = false;
+       	}
+ 		 if(document.getElementById("16").checked){
+     	 document.getElementById("16").disabled = false;
+     	}
+ 		 if(document.getElementById("17").checked){
+     	 document.getElementById("17").disabled = false;
+     	}
+		  if(document.getElementById("18").checked){
+     	 document.getElementById("18").disabled = false;
+     	}
+	}
+
